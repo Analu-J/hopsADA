75cac0535e070159627605de978cc4d612fafd7c
Analu Jahi <ajjahi1208@gmail.com>
2025-01-24 13:51:07 -0500

    messing around

--- hopsADA/GameScene.swift
+++ hopsADA/GameScene.swift
@@ -9,100 +9,145 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+    private var character: SKSpriteNode!
+    private var platforms: [SKSpriteNode] = []
+    private var touchLocation: CGFloat?
+    private var isGameStarted = false
+    private var startLabel: SKLabelNode!
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    override func didMove(to view: SKView) {
+        //background
+        self.backgroundColor = .blue
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+
+    //(placeholder character)
+        character = SKSpriteNode(imageNamed: "Hops")
+        character.position = CGPoint(x: self.frame.midX, y: self.frame.minY + 100)
+        character.physicsBody?.allowsRotation = false
+        character.physicsBody?.restitution = 1.0
+        character.physicsBody?.friction = 0.0
+        character.physicsBody?.linearDamping = 0.0
+        character.physicsBody?.affectedByGravity = true
+        character.physicsBody?.isDynamic = false
+        var location = String()
+
+        self.addChild(character)
+
+     
+        createPlatform(at: CGPoint(x: self.frame.midX, y: self.frame.minY + 50))
+        createPlatform(at: CGPoint(x: self.frame.midX - 100, y: self.frame.minY + 150))
+        createPlatform(at: CGPoint(x: self.frame.midX + 100, y: self.frame.minY + 250))
+
+      
+        startLabel = SKLabelNode(text: "Tap to Start")
+        startLabel.fontName = "AvenirNext-Bold"
+        startLabel.fontSize = 40
+        startLabel.fontColor = .white
+        startLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        self.addChild(startLabel)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    private func createPlatform(at position: CGPoint) {
+        let platform = SKSpriteNode(color: .green, size: CGSize(width: 100, height: 20))
+        platform.position = position
+        platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
+        platform.physicsBody?.isDynamic = false
+        platform.physicsBody?.friction = 0.0
+        self.addChild(platform)
+        platforms.append(platform)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    private func gameOver() {
+        character.removeFromParent()
+        let gameOverLabel = SKLabelNode(text: "Game Over")
+        gameOverLabel.fontName = "AvenirNext-Bold"
+        gameOverLabel.fontSize = 50
+        gameOverLabel.fontColor = .red
+        gameOverLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        self.addChild(gameOverLabel)
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let touch = touches.first else { return }
+
+        if !isGameStarted {
+           // game start
+            isGameStarted = true
+            character.physicsBody?.isDynamic = true
+            character.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 300))
+            startLabel.removeFromParent()
+        } else {
+            
+            touchLocation = touch.location(in: self).x
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let touch = touches.first else { return }
+        touchLocation = touch.location(in: self).x
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        touchLocation = nil
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        guard isGameStarted else { return }
+
+      
+        if character.position.y <= self.frame.minY {
+            gameOver()
+            isGameStarted = false
+            return
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
+
+     
+        if character.position.x < self.frame.minX {
+            character.position.x = self.frame.maxX
+        } else if character.position.x > self.frame.maxX {
+            character.position.x = self.frame.minX
         }
+
+       
+        if let touchLocation = touchLocation {
+            let dx = touchLocation - character.position.x
+            character.physicsBody?.velocity.dx = dx * 5
+        }
+
+      
+        for platform in platforms {
+            if platform.position.y < self.frame.minY + 20 {
+                platform.position.y = self.frame.minY + 20
+            }
+        }
+
+      
+        for platform in platforms {
+            if platform.position.y < self.frame.minY {
+                platform.removeFromParent()
+                if let index = platforms.firstIndex(of: platform) {
+                    platforms.remove(at: index)
+                }
+
+                let newPlatformY = platforms.last?.position.y ?? self.frame.minY + 300
+                createPlatform(at: CGPoint(x: CGFloat.random(in: self.frame.minX...self.frame.maxX), y: newPlatformY + 100))
+            }
+        }
+
+  
+        if character.physicsBody?.velocity.dy ?? 0 <= 0 {
+            for platform in platforms {
+                if character.frame.intersects(platform.frame) {
+                    character.physicsBody?.velocity.dy = 600
+                    break
+                }
+            }
+        }
+
         
+        if character.physicsBody?.velocity.dy ?? 0 > 600 {
+            character.physicsBody?.velocity.dy = 600
+        }
     }
 }
+
